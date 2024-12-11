d5e4c3c6c22b2fee38791c5f8b4a8ab568d06c61
Aashish_Nepal <aashishnepal64@gmail.com>
2024-12-11 17:00:26 +0545

    Updated

--- includes/header.php
+++ includes/header.php
@@ -6,29 +6,30 @@
     <title>Bibhuti Solutions</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
-    <link rel="stylesheet" href="../assets/css/header.css">
+    <link rel="stylesheet" href="../assets/css/headers.css">
     <style>
         /* Add custom styles here if needed */
     </style>
 </head>
 <body>
 
-    <div class="contact-bar d-flex flex-wrap justify-content-between align-items-center px-3 py-2">
-        <div class="contact-info text-center text-lg-start mb-2 mb-lg-0">
-            <span class="me-3 d-block d-lg-inline">
-                <i class="fas fa-phone-alt"></i> 0480 333 779
-            </span>
-            <span class="d-none d-lg-inline">|</span>
-            <a href="mailto:contact@bihubsolutions.com.au" class="ms-lg-3 d-block d-lg-inline">
-                <i class="bi bi-envelope"></i> contact@bihubsolutions.com.au
-            </a>
-        </div>
-        <div class="social-icons text-center">
-            <a href="https://facebook.com" target="_blank" class="text-white">
-                <i class="bi bi-facebook"></i>
-            </a>
-        </div>
+<div class="contact-bar d-flex flex-wrap justify-content-between align-items-center px-3 py-2">
+    <div class="contact-info text-center text-lg-start mb-2 mb-lg-0">
+        <span class="me-3 d-block d-lg-inline">
+            <i class="fas fa-phone-alt"></i> 0480 333 779
+        </span>
+        <span class="d-none d-lg-inline">|</span>
+        <a href="mailto:contact@bihubsolutions.com.au" class="ms-lg-3 d-block d-lg-inline">
+            <i class="bi bi-envelope"></i> contact@bihubsolutions.com.au
+        </a>
     </div>
+    <div class="social-icons text-center">
+        <a href="https://facebook.com" target="_blank" class="text-white">
+            <i class="bi bi-facebook"></i>
+        </a>
+    </div>
+</div>
+
     <nav class="custom-navbar navbar navbar-expand-lg overflow-hidden">
       <div class="container-fluid">
        <div class="logocover">
